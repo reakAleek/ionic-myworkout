90da55b1200b5dcc6cdd8795f299e6702f8a47b1
Jan Calanog <if15b042@technikum-wien.at>
2017-06-08 21:26:02 +0200

    Added Edit Feature

--- src/pages/new-workout/new-workout.ts
+++ src/pages/new-workout/new-workout.ts
@@ -22,6 +22,7 @@ export class NewWorkoutPage {
   private workoutForm: FormGroup;
   private sets: FormArray;
   private durations: string[] = [];
+  private id: number;
 
   constructor(
     private workoutService: WorkoutService,
@@ -31,6 +32,20 @@ export class NewWorkoutPage {
   ) {
     this.workoutForm = this.initForm();
     this.sets = <FormArray>this.workoutForm.controls['sets'];
+
+    console.log(navParams.get("id"));
+
+    if (navParams.get("id") !== undefined) {
+      this.workoutService.getWorkout(navParams.get("id")).subscribe(data => {
+        console.log(data.name);
+        this.workoutForm.get("name").setValue(data.name);
+        data.sets.forEach((i,index) => {
+          this.addSet(i);
+          this.durations[index] = i.duration;
+        });
+        this.id = data.id;
+      });
+    }
   }
 
   ionViewDidLoad() {
@@ -57,12 +72,12 @@ export class NewWorkoutPage {
   initSet(set: Set = null) {
 
     let result = this.formBuilder.group({
-      exercise: this.initExercise(),
+      exercise: this.initExercise(set.exercise),
       duration: this.generateDuration(0,0),
     });
 
     if (set != null) {
-      result.get("exercise").setValue(set.exercise);
+      result.get("duration").setValue(set.duration);
     }
 
     return result;
@@ -98,9 +113,19 @@ export class NewWorkoutPage {
         i.duration = this.generateDuration(0,0);
       }
     });
-    this.workoutService.addWorkout(workout).subscribe(
-      success => { this.navCtrl.pop() },
-      error => { console.log(error) }
-    );
+
+    if (this.id != null) {
+      workout.id = this.id;
+      this.workoutService.saveWorkout(workout).subscribe(
+        success => { this.navCtrl.pop() },
+        error => { console.log(error); }
+      );
+    }
+    else {
+      this.workoutService.addWorkout(workout).subscribe(
+        success => { this.navCtrl.pop() },
+        error => { console.log(error) }
+      );
+    }
   }
 }
